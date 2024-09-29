19afbcc588740e200803e94b7bd3c4806c9edfeb
Gust4v0M <gustavomorgado6@gmail.com>
2024-09-29 00:09:05 -0300

    Criando Material Table para Listar Cursos

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Courses } from './model/courses';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+
+  courses: Courses[] = [
+    {id: '1', nome: 'Angular', category: 'front-end'}
+  ];
+  displayedColumns = ['name', 'category']
 
 }
---
+++ src/app/courses/model/courses.ts
@@ -0,0 +1,5 @@
+export interface Courses {
+  id: string;
+  nome: string;
+  category: string;
+}
