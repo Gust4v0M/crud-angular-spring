4353115e3f3bf4000829d8a73e438a45034a85c3
Gust4v0M <gustavomorgado6@gmail.com>
2024-09-29 00:22:21 -0300

    CSS do Material Table

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { Courses } from './model/courses';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatCardModule,MatToolbarModule ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
